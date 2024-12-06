2eb60208afdaee956df3969b0b0cac99906e7413
rgborgeaud <gael.borgeaud@heig-vd.ch>
2024-12-06 18:45:21 +0100

    Added SMTP messages and connection handlers. TODO : encoding of From and To fields in Data.

--- src/main/java/rgborgeaud_Svelva/ch/dai/lab/smtp/Client/Client.java
+++ src/main/java/rgborgeaud_Svelva/ch/dai/lab/smtp/Client/Client.java
@@ -1,5 +1,38 @@
 package rgborgeaud_Svelva.ch.dai.lab.smtp.Client;
 
 
+import java.io.*;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Client {
+
+    static final int MAIL_DEV_PORT = 1025;
+    static final String MAIL_DEV_HOST = "localhost";
+
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket(MAIL_DEV_HOST, MAIL_DEV_PORT);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(socket.getInputStream(), UTF_8));
+             BufferedWriter out = new BufferedWriter(
+                     new OutputStreamWriter(socket.getOutputStream(), UTF_8))
+        ) {
+
+            String sender = "gael.borgeaud@gmail.com";
+            String[] recipients = {"mylene.farmer@baguette.fr", "johnny.hallyday@pinard.com", "roger.federer@fondue.ch",};
+            String subjet = "Truite de compagnie à vendre";
+            String text = "Contactez-moi au plus vite et évitez, à partir d'aujourd'hui, les caractères spéciaux " +
+                    "tels que : ééààèèüüääöö!!££$$¨^^@##|¬&, merci d'avance";
+
+            var smtpMessages = new SMTPMessagesHandler(sender, recipients, subjet, text);
+            var smtpConnection = new SMTPConnectionHandler(in, out, smtpMessages);
+
+            smtpConnection.handle();
+
+        } catch (IOException e) {
+            System.out.println("Error : " + e);
+        }
+    }
 }
